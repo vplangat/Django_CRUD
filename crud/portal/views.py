0aba9e97184ce0cbaf5199508b0a7f14d50a9696
Vinny <vinny@Vinnys-MacBook-Air.local>
2022-07-20 00:17:26 +0300

    Linked models to views

--- crud/portal/views.py
+++ crud/portal/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 
 def dashboard(request):
-    return render(request, 'portal/dashboard.html')
+    total_customers = Customer.objects.all()
+    total_products = Product.objects.all()
+    total_orders = Order.objects.all()
+    context = {'customers': total_customers, 'products': total_products, 'orders': total_orders}
+    return render(request, 'portal/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'portal/products.html')
+    total_products = Product.objects.all()
+    return render(request, 'portal/products.html', {'products': total_products})
 
 
 def customers(request):
